b8aec98c4d27d7ae72710a9f9aeda2e1bebba421
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-11-20 13:20:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# color-cups
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/gonzalote99/color-cups)
---
+++ script.js
@@ -0,0 +1,17 @@
+let kubki = document.querySelectorAll('.cup');
+
+function klik(evt) {
+  let poziom = Number(this.getAttribute("poziom", "0"));
+  poziom += 23,5;
+  if(poziom < 99) {
+    this.style.backgroundPosition = "0 " + poziom + "%";
+    this.setAttribute("poziom", poziom);
+  }
+  else {
+    this.style.cursor = "not-allowed"
+  }
+}
+
+kubki.forEach(function(kubek) {
+  kubek.addEventListener('click', klik);
+})
